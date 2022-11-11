03d25ec8b2ce116a4f806fe087b1f82419336e83
Little <littlepriceonu@icloud.com>
2022-11-10 21:28:44 -0800

    a couple edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    backgroundSize: {
+      gradientSize: '400% 400%'
+    },
     extend: {
+      keyframes: {
+        scrollingGradientframes: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%' : { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        }
+      },
+
+      animation: {
+        scrollingGradient: 'scrollingGradientframes 15s ease infinite',
+        fastScrollingGradient: 'scrollingGradientframes 5s ease infinite'
+      }
     },
   },
     plugins: [
