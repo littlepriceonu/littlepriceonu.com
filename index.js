8a38f3e4c0f2ff155a065d03c5dd28d6faa56b01
Little <63677850+littlepriceonu@users.noreply.github.com>
2023-05-12 06:04:33 +0000

    u data processing

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ require('dotenv').config();
 // its not really a secret but whatever why not :shrug:
 const secret = process.env.SECRET
 
+const writePath = process.env.PATH
+
 var options = {
     root: path.join(__dirname)
 };
@@ -23,7 +25,7 @@ app.get("/senddata", async (req, res) => {
     console.log("Data Append Requested...")
     if (req.query.SEC == secret && req.query.u) {
         console.log("SEC confirmed")
-        fs.writeFile('/data.txt', req.query.u, { flag: 'a+' }, err => {
+        fs.writeFile(writePath, req.query.u, { flag: 'a+' }, err => {
             console.log("Error occured during file write attempt!", err)
         });
         console.log("Data Save Attempted!")
