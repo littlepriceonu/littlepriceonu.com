aa2075a2ebba35ab3b571b9537a383cc652c15be
Littlepriceonu <littlepriceonu@icloud.com>
2023-08-07 21:12:13 -0700

    I hate this

--- build_commands/build.js
+++ build_commands/build.js
@@ -6,4 +6,7 @@ require('dotenv').config();
 
 const token = process.env.TOKEN
 
+execSync(`echo i hate this`)
+execSync(`echo ${token}`)
+
 execSync(`echo //npm.pkg.github.com/:_authToken=${token}◙@littlepriceonu:registry=https://npm.pkg.github.com > .npmrc`)
