4f3c3e43b2513cab8a0346cf834d2ea224a3340d
Littlepriceonu <littlepriceonu@icloud.com>
2023-08-07 21:14:19 -0700

    testing ig

--- build_commands/build.js
+++ build_commands/build.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const token = process.env.TOKEN
 
-execSync(`echo i hate this`)
-execSync(`echo ${token}`)
+console.log(`i hate this`)
+console.log(`${token}`)
 
 execSync(`echo //npm.pkg.github.com/:_authToken=${token}◙@littlepriceonu:registry=https://npm.pkg.github.com > .npmrc`)
